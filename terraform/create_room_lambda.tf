78bf5ae6dd108dfe9f11c93cd71bc7f07d986f0d
Mike Wilkinson <mwilkinson@imo-online.com>
2019-01-07 21:36:09 -0600

    Name terraform file to be lambda specific

--- terraform/create_room_lambda.tf
+++ terraform/create_room_lambda.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "signal_lambda" {
+data "aws_iam_policy_document" "create_room_lambda" {
   statement {
     actions = [
       "sts:AssumeRole",
@@ -11,7 +11,7 @@ data "aws_iam_policy_document" "signal_lambda" {
   }
 }
 
-data "aws_iam_policy_document" "signal_lambda_access_doc" {
+data "aws_iam_policy_document" "create_room_lambda_access_doc" {
   statement {
     actions = [
       "dynamodb:BatchGetItem",
@@ -67,26 +67,26 @@ data "aws_iam_policy_document" "signal_lambda_access_doc" {
   }
 }
 
-resource "aws_iam_policy" "signal_lambda_access" {
-  name   = "signalws_lambda_access"
-  policy = "${data.aws_iam_policy_document.signal_lambda_access_doc.json}"
+resource "aws_iam_policy" "create_room_lambda_access" {
+  name   = "create_room_lambda_access"
+  policy = "${data.aws_iam_policy_document.create_room_lambda_access_doc.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "signal_lambda_dynamo_policy_attach" {
-  role       = "${aws_iam_role.iam_for_signal_lambda.name}"
-  policy_arn = "${aws_iam_policy.signal_lambda_access.arn}"
+  role       = "${aws_iam_role.iam_for_create_room_lambda.name}"
+  policy_arn = "${aws_iam_policy.create_room_lambda_access.arn}"
 }
 
-resource "aws_iam_role" "iam_for_signal_lambda" {
-  name               = "signalws_iam"
-  assume_role_policy = "${data.aws_iam_policy_document.signal_lambda.json}"
+resource "aws_iam_role" "iam_for_create_room_lambda" {
+  name               = "create_room_iam"
+  assume_role_policy = "${data.aws_iam_policy_document.create_room_lambda.json}"
 }
 
-resource "aws_lambda_function" "signal-ws" {
-  filename         = "signal.zip"
-  function_name    = "signalws"
+resource "aws_lambda_function" "create_room" {
+  filename         = "create_room.zip"
+  function_name    = "create_room"
   runtime          = "python3.6"
-  handler          = "main.lambda_handler"
-  role             = "${aws_iam_role.iam_for_signal_lambda.arn}"
-  source_code_hash = "${base64sha256(file("signal.zip"))}"
+  handler          = "create_room.lambda_handler"
+  role             = "${aws_iam_role.iam_for_create_room_lambda.arn}"
+  source_code_hash = "${base64sha256(file("create_room.zip"))}"
 }
